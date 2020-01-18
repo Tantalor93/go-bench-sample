20d722011d5607f33e4a2a7945c304c05009dbe3
Ondrej Benkovsky <ondrej.benkovsky@wandera.com>
2020-01-19 00:20:48 +0100

    add test

---
+++ pkg/utils/str_test.go
@@ -0,0 +1,32 @@
+package utils
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"should reverse",
+			args{s: "ahoj"},
+			"joha",
+		},
+		{
+			"should handle empty string",
+			args{s: ""},
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Reverse(tt.args.s); got != tt.want {
+				t.Errorf("Reverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
